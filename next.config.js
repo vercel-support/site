127acf34d1ea27f33b2f7643998366ec9755baff
Brandon Pittman <github@brandonpittman.net>
2020-07-01 12:54:09 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 const withPlugins = require("next-compose-plugins");
 const withSvgr = require("next-svgr");
-const withMdxEnhanced = require("next-mdx-enhanced");
-const readingTime = require("reading-time");
 
-const withMdx = withMdxEnhanced({
-  layoutPath: "layouts",
-  defaultLayout: true,
-  fileExtensions: ["mdx"],
-  remarkPlugins: [],
+const withMdx = require("@next/mdx")({
   rehypePlugins: [require("@mapbox/rehype-prism")],
-  extendFrontMatter: {
-    process: (mdxContent, frontMatter) => {
-      return {
-        timeToRead: readingTime(mdxContent).text,
-      };
-    },
-  },
 });
 
 module.exports = withPlugins([withMdx, withSvgr], {
-  // pageExtensions: ["js", "jsx", "mdx"]
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
 });
