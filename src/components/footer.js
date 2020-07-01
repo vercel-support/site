4712b6a043d3c35f6f68e866fd5e58bfb55a8f52
Brandon Pittman <github@brandonpittman.net>
2020-07-01 23:09:20 +0900

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="flex justify-center py-16 text-sm text-gray-700">
       <span>Copyright © {new Date().getFullYear()}.&nbsp;</span>
 
-      <a href="https://gatsbyjs.org" target="_blank" rel="noreferrer noopener">
+      <a href="https://nextjs.org" target="_blank" rel="noreferrer noopener">
         Powered by Next.js.
       </a>
     </footer>
