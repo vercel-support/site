2933cffc89dcc73812998c49e5e25c22e3ffd9f7
Brandon Pittman <blp@hey.com>
2020-07-23 15:03:48 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPWA = require("next-pwa")({
   pwa: {
+    disable: process.env.NODE_ENV === "development",
     dest: "public"
   }
 });
--- src/components/media_card.js
+++ src/components/media_card.js
@@ -1,17 +1,16 @@
 import Card from "./card";
 // import Img from "react-optimized-image";
 
-const MediaCard = ({ children, title }) => {
+const MediaCard = ({ image, children, title }) => {
   return (
     <Card className="relative w-full transition-all ease-in-out duration-200 group transform hover:translate-y-px hover:shadow-md">
-      {/* <div>
-        <Img
-          webp
-          sizes={[480]}
-          src={require(`../../content/posts/images/${image}`)}
-          className="object-cover w-full h-64"
-        />
-      </div> */}
+      {/* <div>                                                    */}
+      {/*   <Img                                                   */}
+      {/*     webp                                                 */}
+      {/*     src={require(`../../content/posts/images/${image}`)} */}
+      {/*     className="object-cover w-full h-64"                 */}
+      {/*   />                                                     */}
+      {/* </div>                                                   */}
 
       <div className="flex flex-col flex-1 p-6">
         {title}
