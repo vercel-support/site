08d0b28bd75cc003dc846d843961130b5ef1c42c
Brandon Pittman <blp@hey.com>
2020-08-11 10:50:53 +0900

    wip

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ReactComponent as TagIcon } from "../images/icon-tag.svg";
+import TagIcon from "@images/icon-tag.svg";
+import { Svg } from "react-optimized-image";
 import PostCard from "../components/post_card";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -14,12 +15,12 @@ const TagTemplate = ({ pageContext, data }) => {
       />
 
       <h1 className="flex items-end text-2xl">
-        <TagIcon className="block w-8 h-8 mr-2" />
+        <Svg src={TagIcon} className="block w-8 h-8 mr-2" />
         Posts tagged with "{pageContext.tag}"
       </h1>
 
       <ul className="flex flex-wrap -mx-4">
-        {data.posts.nodes.map((post) => (
+        {data.posts.nodes.map(post => (
           <li
             key={post.frontmatter.slug}
             className="flex w-full p-4 mt-4 md:w-1/2 lg:w-1/3"
