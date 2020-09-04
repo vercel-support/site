f84a4ca604fc3e533f91715e9164e398dd697926
Brandon Pittman <blp@hey.com>
2020-09-04 13:39:15 +0900

    📝 gitignore

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 const withPWA = require("next-pwa")({
   pwa: {
     disable: process.env.NODE_ENV === "development",
-    dest: "public"
-  }
+    dest: "public",
+  },
 });
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-optimized-images");
 const withMdx = require("@next/mdx")({
-  rehypePlugins: [require("@mapbox/rehype-prism")]
+  rehypePlugins: [require("@mapbox/rehype-prism")],
 });
 
 module.exports = withPlugins([withPWA, withMdx, withImages], {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx", "bs.js"],
   async redirects() {
     return [
       {
         source: "/posts/(.*)",
         destination: "/writing/$1",
-        permanent: true
+        permanent: true,
       },
       {
         source: "/here",
         destination: "/",
-        permanent: true
-      }
+        permanent: true,
+      },
     ];
-  }
+  },
 });
---
+++ src/components/footer.re
@@ -0,0 +1,21 @@
+let today = Js.Date.make();
+let thisYear = Js.Date.getFullYear(today);
+let thisYearAsString = Js.Float.toString(thisYear);
+let copy = {js|©|js};
+let nbsp = {js|\u00A0|js};
+let s = ReasonReact.string;
+
+[@react.component]
+let make = () =>
+  <footer className="flex justify-center py-16 text-sm text-gray-700">
+    <span>
+      {("Copyright " ++ copy ++ thisYearAsString ++ "." ++ nbsp)->s}
+    </span>
+    <a
+      href="https://nextjs.org"
+      target="_blank"
+      rel="noreferrer noopener"
+      className="underline text-key">
+      "Powered by Next.js."->s
+    </a>
+  </footer>;
