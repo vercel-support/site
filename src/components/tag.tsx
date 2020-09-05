d808a80ea7f1e0a10113d15428664a034f87c36b
Brandon Pittman <blp@hey.com>
2020-09-05 20:45:19 +0900

    1.1.3

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import TagIcon from "@images/icon-tag.svg";
 import { Svg } from "react-optimized-image";
-import PostCard from "../components/post_card";
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import Article from "@components/article";
+import Layout from "@components/layout";
+import SEO from "@components/seo";
 
 export default function TagTemplate({ pageContext, data }) {
   return (
@@ -24,7 +24,7 @@ export default function TagTemplate({ pageContext, data }) {
             key={post.frontmatter.slug}
             className="flex w-full p-4 mt-4 md:w-1/2 lg:w-1/3"
           >
-            <PostCard post={post} />
+            <Article post={post} />
           </li>
         ))}
       </ul>
