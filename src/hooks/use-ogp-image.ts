e937148bf1412aaa3a4f213921f1613e3ada84e6
Brandon Pittman <blp@hey.com>
2020-09-12 16:53:59 +0900

    Update use-ogp-image.ts

--- src/hooks/use-ogp-image.ts
+++ src/hooks/use-ogp-image.ts
@@ -1,3 +1,3 @@
-export default function useOgpImage(title: string) {
-  return `https://blp-og-image.vercel.app/${encodeURI(title)}?theme=dark`;
+export default function useOgpImage(title: string, theme = "dark") {
+  return `https://blp-og-image.vercel.app/${encodeURI(title)}?theme=${theme}`;
 }
